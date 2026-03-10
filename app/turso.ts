ea6a6f7cc76723e4c2ae059fa5c880255d3e2dce
CodingGaby <macma.cube1@gmail.com>
2026-03-10 11:17:37 -0600

    fix(app): Best client implementation 🛠️
    Added checks for url and token from turso

--- app/turso.ts
+++ app/turso.ts
@@ -1,5 +1,13 @@
 import { createClient } from "@libsql/client";
 
+if (!process.env.TURSO_DATABASE_URL) {
+  throw new Error("TURSO_DATABASE_URL no está definida");
+}
+
+if (!process.env.TURSO_AUTH_TOKEN) {
+  throw new Error("TURSO_AUTH_TOKEN no está definida");
+}
+
 export const turso = createClient({
   url: process.env.TURSO_DATABASE_URL,
   authToken: process.env.TURSO_AUTH_TOKEN,
